fb8453ec4f42ec471d57cbd29f5a231eee9c37fb
helemalmeida <helemsmalmeida@gmail.com>
2022-07-18 15:40:59 -0300

    Initial commit

---
+++ aulas.js
@@ -0,0 +1,141 @@
+//============================VARIAVEIS====================================
+
+// let idade = 27;
+// let nomeCompleto = "Helem Almeida";
+// let altura = 1.51;
+// console.log(idade, nomeCompleto, altura);
+
+//============================CONSTANTES====================================
+
+// let valorIngressoAdulto = 20;
+// valorIngressoAdulto = 45;
+// console.log(valorIngressoAdulto);
+
+//usar o const em valores fixos, se precisar alterar valores usar o let
+
+// const valorIngressoAdulto = 20;
+// console.log(valorIngressoAdulto);
+
+//============================TIPOS PRIMITIVOS====================================
+
+// let nome = 'Helem'; //string literal
+// let idade = 23.7; //number literal
+// let estaAprovado = true; //boolean
+// let sobrenome = undefined; //undefined
+// let corSelecionado = null; //redefinir um valor 
+
+//============================OBJETOS====================================
+
+// let nome = 'Helem';
+// let sobrenome = undefined; 
+// let idade = 27; 
+// let estaAprovado = true; 
+// let altura = 1.51;
+// let statusRelacionamento = 'Namorando';
+// let genero = 'F';
+
+// // let pessoa = { } ; //objeto vazio
+
+// let pessoa = {
+//     nome: 'Helem',
+//     sobrenome: 'Almeida',
+//     idade: 27,
+//     estaAprovado: true,
+//     altura: 1.51,
+//     statusRelacionamento: 'Namorando',
+//     genero: 'F'
+// };
+
+// console.log(pessoa);
+
+//============================ARRAY====================================
+
+// let familiaIdade = [18,22,25,26,44,63];
+// console.log(familiaIdade[3]);
+
+// let baby = [26, 'Thaís Brunelli','Itabira','Jornalista'];
+// console.log(baby[1]);
+
+//============================FUNCTION====================================
+
+//nomear função - PADRÃO: verbo+substantivo 
+
+// let corSite = "azul";
+
+// function resetaCor () { //sem parametro
+//     corSite=""; 
+// };
+
+// console.log(corSite);
+
+// resetaCor();
+
+// console.log(corSite);
+
+
+// function resetaCor (cor,tonalidade) { //com parametro
+//     corSite = cor + ' ' + tonalidade;
+// };
+
+// console.log(corSite);
+
+// resetaCor("rosa","claro");
+
+// console.log(corSite);
+
+
+// function resetaCor (cor,tonalidade) {
+//     corSite = cor + ' ' + tonalidade;
+// };
+
+// console.log(corSite);
+
+// resetaCor("verde","claro");
+
+// console.log(corSite);
+
+//============================ TIPOS DE FUNÇÕES ====================================
+//1 tipos
+
+//realiza uma tarefa e não retorna nada
+
+// function dizerNome () {
+//     console.log('Helem');
+// }
+// dizerNome();
+
+// //faz um calculo/operação e retorna 
+
+// function multiplicarValor (valor) {
+//     return valor * 2;
+// }
+
+// let resultado = multiplicarValor (7);
+// console.log(resultado);
+
+//============================ OPERADORES ====================================
+
+//operadores aritmeticos [matematicos]
+
+
+
+//operadores atribuição
+
+
+
+
+//operadores comparação
+
+
+
+
+//operadores logicos
+
+
+
+
+//operadores bitwise
+
+
+
+
